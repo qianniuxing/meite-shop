ca6459b672fce4d296079f2a149804f14c29f4bd
niu <niu_jw@163.com>
2019-11-20 22:41:10 +0800

    xxl-sso-server

---
+++ shop-portal/pay-web/src/main/java/com/hello/controller/PayController.java
@@ -0,0 +1,14 @@
+package com.hello.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PayController {
+
+    @RequestMapping("indes")
+    public String indes() {
+        return "主页";
+    }
+}
